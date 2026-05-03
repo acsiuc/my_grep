4ab62b339f538fb8d8359b6714c72d2ba3014174
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-03 17:33:05 +0300

    changed from reading the whole file at once, to reading it line by line and added and exception for when encountering UnicodeDecodeError

--- grep.py
+++ grep.py
@@ -5,6 +5,14 @@ for root, dirs, files in os.walk(u'C:\\Users\\Axiuc\\Downloads'):
     print('Current Directory: ', root)
     print('Subidrectories: ', dirs)
     print('Files:', files)
+    for x in files:
+        to_read = os.path.join(root, x)
+        try:
+            with open(to_read, "r") as f:
+                for line in f:
+                    print(line)
+        except UnicodeDecodeError:
+            continue
 
 # for root,dirs, files in in files:
 #     with open(os.path.join(f'C:\\Users\\Axiuc\\Downloads\\{dirs}', x), 'r') as f:
