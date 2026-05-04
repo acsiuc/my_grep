81a684690025a427c8b14d16e00d61c143bf4825
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-04 13:28:25 +0300

    added -n and -i flag functionality, code refactoring, and will try to not repeat myself in following flag

--- src/my_grep/core.py
+++ src/my_grep/core.py
@@ -12,16 +12,29 @@ for x in sys.argv[1:]:
 print(flags)
 print(arguments)
 
-user_path = arguments[-1]
-
-for root, dirs, files in os.walk(user_path):
-    for x in files:
-        to_read = os.path.join(root, x)
-        try:
-            with open(to_read, "r") as f:
-                for line_number, line in enumerate(f):
-                    if arguments[0] in line:
-                        print(line_number, line)
-        except UnicodeDecodeError:
-            continue
+if arguments:
+    if len(arguments)>1:
+        user_path = arguments[-1]
+    elif len(arguments) == 1:
+        user_path = '.'
+    for root, dirs, files in os.walk(user_path):
+        for x in files:
+            to_read = os.path.join(root, x)
+            try:
+                with open(to_read, "r") as f:
+                    for line_number, line in enumerate(f):
+                        if '-i' in flags:
+                            if arguments[0].lower() in line.lower():
+                                if '-n' in flags:
+                                    print(line_number,line,f'found in {to_read}')
+                                elif arguments[0].lower() in line.lower():
+                                    print(line,f'found in {to_read}')
+                        else:
+                            if arguments[0] in line :
+                                if '-n' in flags:
+                                    print(line_number,line,f'found in {to_read}')
+                                elif arguments[0] in line:
+                                    print(line,f'found in {to_read}')
+            except UnicodeDecodeError:
+                continue
 
