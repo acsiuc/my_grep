8a1a6ce91ffe7e0675713682cee136d31f4ff939
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-04 20:18:04 +0300

    added -r flag functionality, refactored from using print to using click. echo()'

--- src/my_grep/core.py
+++ src/my_grep/core.py
@@ -4,29 +4,35 @@ import click
 
 @click.command()
 @click.option('-i', is_flag=True, help ='Case insensitive searching of keyword')
+# @click.option('-v', is_flag=True, help ='All words in the line, except the user input keyword')
+@click.option('-r', is_flag=True, help ='Iteratively searching through all subdirectories of root_path')
 @click.option('-n', is_flag=True, help ='Gives the number of the line')
 @click.argument('word')
 @click.argument('user_path')
 
-def search(word, user_path,i,n):
+def search(word, user_path,i,n,r):
     for root, dirs, files in os.walk(user_path):
         for x in files:
             to_read = os.path.join(root, x)
             try:
                 with open(to_read, "r") as f:
                     for line_number, line in enumerate(f):
-                        if i is True:
+                        if i:
                             if word.lower() in line.lower():
-                                if n is True:
-                                    print(line_number,line,f'found in {to_read}')
+                                if n:
+                                    click.echo(f"{line_number} {line} found in {to_read}")
                                 else:
-                                    print(line,f'found in {to_read}')
+                                    click.echo(f"{line} found in {to_read}")
                         else:
                             if word in line :
-                                if n is True:
-                                    print(line_number,line,f'found in {to_read}')
+                                if n:
+                                    click.echo(f"{line_number} {line} found in {to_read}")
                                 else:
-                                    print(line,f'found in {to_read}')
+                                    click.echo(f"{line} found in {to_read}")
             except UnicodeDecodeError:
                 continue
+        if not r:
+            break
 
+if __name__ == '__main__':
+    search()
