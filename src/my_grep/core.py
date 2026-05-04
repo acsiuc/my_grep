51d49fcaeab310215e770cfe9f55a1ac28c1a755
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-04 19:36:19 +0300

    Refactored command line aprsing to use click library

--- src/my_grep/core.py
+++ src/my_grep/core.py
@@ -8,22 +8,22 @@ import click
 @click.argument('word')
 @click.argument('user_path')
 
-def search(word, user_path):
+def search(word, user_path,i,n):
     for root, dirs, files in os.walk(user_path):
         for x in files:
             to_read = os.path.join(root, x)
             try:
                 with open(to_read, "r") as f:
                     for line_number, line in enumerate(f):
-                        if '-i' in flags:
-                            if arguments[0].lower() in line.lower():
-                                if '-n' in flags:
+                        if i is True:
+                            if word.lower() in line.lower():
+                                if n is True:
                                     print(line_number,line,f'found in {to_read}')
                                 else:
                                     print(line,f'found in {to_read}')
                         else:
                             if word in line :
-                                if '-n' in flags:
+                                if n is True:
                                     print(line_number,line,f'found in {to_read}')
                                 else:
                                     print(line,f'found in {to_read}')
