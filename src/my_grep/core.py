538ec4467b5021c3cf59b0f524417f86c883b879
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-06 15:54:38 +0300

    refactored coe to be cleaner and split into functions that take different task instead of the tasks being overlapped in a singular function

--- src/my_grep/core.py
+++ src/my_grep/core.py
@@ -10,29 +10,35 @@ import click
 @click.argument('word')
 @click.argument('user_path')
 
-def search(word, user_path,i,n,r):
+def split_and_search(user_path, word,i,n,r):
     for root, dirs, files in os.walk(user_path):
         for x in files:
             to_read = os.path.join(root, x)
-            try:
-                with open(to_read, "r") as f:
-                    for line_number, line in enumerate(f):
-                        if i:
-                            if word.lower() in line.lower():
-                                if n:
-                                    click.echo(f"{line_number} {line} found in {to_read}")
-                                else:
-                                    click.echo(f"{line} found in {to_read}")
-                        else:
-                            if word in line :
-                                if n:
-                                    click.echo(f"{line_number} {line} found in {to_read}")
-                                else:
-                                    click.echo(f"{line} found in {to_read}")
-            except UnicodeDecodeError:
-                continue
-        if not r:
+            search(to_read, word,i,n)
+    
+        if not r: 
             break
 
+def search(to_read,word,i,n):
+    try:        
+        with open(to_read, "r") as f:
+            for line_number, line in enumerate(f):
+                if i:
+                    if word.lower() in line.lower():
+                        if n:
+                            click.echo(f"{line_number} {line} found in {to_read}")
+                        else:
+                            click.echo(f"{line} found in {to_read}")
+                else:
+                    if word in line :
+                        if n:
+                            click.echo(f"{line_number} {line} found in {to_read}")
+                        else:
+                            click.echo(f"{line} found in {to_read}")
+    except UnicodeDecodeError:
+                continue
+
+
+     
 if __name__ == '__main__':
-    search()
+    split_and_search()
