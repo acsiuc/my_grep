eeceb750d8ae352e186a641592dec0e251a5d9c3
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-06 20:21:21 +0300

    Exception was not being caught correctly and instead was discarding all  other lines in a file when encountering UnicodeDecodeError, the exception now works

--- src/my_grep/core.py
+++ src/my_grep/core.py
@@ -12,31 +12,31 @@ import click
 
 def split_and_search(user_path, word,i,n,r):
     for root, dirs, files in os.walk(user_path):
-        for x in files:
-            to_read = os.path.join(root, x)
-            search(to_read, word,i,n)
-    
-        if not r: 
-            break
+        try:
+            for x in files:
+                to_read = os.path.join(root, x)
+                search(to_read, word,i,n)
+            if not r: 
+                break
+        except UnicodeDecodeError:
+            continue
 
-def search(to_read,word,i,n):
-    try:        
-        with open(to_read, "r") as f:
-            for line_number, line in enumerate(f):
-                if i:
-                    if word.lower() in line.lower():
-                        if n:
-                            click.echo(f"{line_number} {line} found in {to_read}")
-                        else:
-                            click.echo(f"{line} found in {to_read}")
-                else:
-                    if word in line :
-                        if n:
-                            click.echo(f"{line_number} {line} found in {to_read}")
-                        else:
-                            click.echo(f"{line} found in {to_read}")
-    except UnicodeDecodeError:
-                continue
+def search(to_read,word,i,n):      
+    with open(to_read, "r") as f:
+        for line_number, line in enumerate(f):
+            if i:
+                if word.lower() in line.lower():
+                    if n:
+                        click.echo(f"{line_number} {line} found in {to_read}")
+                    else:
+                        click.echo(f"{line} found in {to_read}")
+            else:
+                if word in line :
+                    if n:
+                        click.echo(f"{line_number} {line} found in {to_read}")
+                    else:
+                        click.echo(f"{line} found in {to_read}")
+
 
 
      
